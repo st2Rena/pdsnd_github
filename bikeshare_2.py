e312fb97f5d0715540498deaee79c3d9f70b4c6e
Rena <renagornyak@gmail.com>
2021-03-14 21:33:49 +0200

    Adding the new string in comments for code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,7 +16,8 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). 
+    # HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("Input the city do you want to explore: Chicago, New York or Washington? \n> \n").lower()
         if city in CITY_DATA.keys():
